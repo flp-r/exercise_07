db0af6e00d96bc97580b98dfad1effbed4073745
Rudol Filip (240557) <240557@vutbr.cz>
2024-10-31 11:31:59 +0100

    first commit

---
+++ lecture_7.R
@@ -0,0 +1,78 @@
+rm(list = ls())
+library(Biostrings)
+
+
+
+seq_score <- readDNAStringSet("V:/PRG_BIN/exercise_07/seq_score.fasta", format = "fasta")
+starting_indices <- c(8, 30, 1, 19, 44)
+motiv_length <- 6
+
+
+Score <- function(starting_indices, seq_score, motiv_length){
+  motivs <- DNAStringSet()
+  counter <- 1
+  for (i in starting_indices){
+    seq_cs <- seq_score[counter]
+    motivs <- c(motivs, subseq(seq_cs, start = i, end = i+motiv_length-1))
+  }
+  
+
+  freq_matrix <- consensusMatrix(motivs)
+  
+  max_values <- apply(freq_matrix, 2, max)
+  
+  
+  score <- sum(max_values)
+  return(score)
+  
+}
+
+Score(starting_indices, seq_score, motiv_length)
+
+
+NextLeaf <- function(array_start, L, k){
+  for (i in L:1){
+    if (array_start[i] < k){
+      array_start[i] <- array_start[i] + 1
+      return(array_start)
+    }
+    array_start[i] <- 1
+  }
+  return(array_start)
+}
+
+
+array_start <- c(8, 30, 1, 19, 44)
+L <- length(seq_score)
+k <- length(seq_score[[1]]) - length(array_start) + 1
+
+NextLeaf(array_start, L, k)
+
+
+BFMotifSearch <- function(DNA, t, n, l){
+  s <- rep(1, l)
+  bestScore <- Score(s, DNA, l)
+  bestMotif <- c()
+  while(TRUE){
+    s <- NextLeaf(s, t, n - l + 1)
+    if (Score(s, DNA, l) > bestScore){
+      bestScore <- Score(s, DNA, l)
+      bestMotif <- s
+    }
+    if (all(s == rep(1, l))){
+      return(bestMotif)
+    }
+      
+  }
+}
+
+
+
+
+DNA <- readDNAStringSet("V:/PRG_BIN/exercise_07/seq_motif.fasta", format = "fasta")
+t <- length(DNA)
+n <- length(DNA[[1]])
+l <- 6
+
+
+BFMotifSearch(DNA, t, n, l)
